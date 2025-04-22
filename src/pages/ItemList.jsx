48492df01b7cef5bb8390c2d05e15b5fb649b2f2
macharia <mrmachariaorsam@gmail.com>
2025-04-22 12:28:59 +0300

    lastCommit

--- src/pages/ItemList.jsx
+++ src/pages/ItemList.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import SearchBar from '../components/SearchBar';
-import ItemCard from '../components/ItemCard';
+
 
 function ItemList() {
   const [items, setItems] = useState([]);
--- src/pages/LowStockItems.jsx
+++ src/pages/LowStockItems.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import ItemCard from '../components/ItemCard';
+
 
 function LowStockItems() {
   const [items, setItems] = useState([]);
